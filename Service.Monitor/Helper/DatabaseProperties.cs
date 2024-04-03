46eed047b28d02e225669dc8ca621c49c348e7e3
Talasoftware <dtalahatu@trinitisolutions.nl>
2024-04-03 15:09:20 +0200

    changed environmentvariabel database to monitor

--- Service.Monitor/Helper/DatabaseProperties.cs
+++ Service.Monitor/Helper/DatabaseProperties.cs
@@ -2,8 +2,8 @@
 
 public class DatabaseProperties
 {
-    public static readonly string Database = Environment.GetEnvironmentVariable("DATABASE");
-    public static readonly string ConnectionString = Environment.GetEnvironmentVariable("MONGODB_CONNECTION");
+    public static readonly string Database = "monitor";
+    public static readonly string? ConnectionString = Environment.GetEnvironmentVariable("MONGODB_CONNECTION");
     public static readonly string ViewsTable = "ActionsPerBusiness";
     public static readonly string EventsTable = "monitor";
 }
